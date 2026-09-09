0507f58aad6b64d7ff3390b9036a9984be2ee763
Shubhi-Sri <shubhisrivastava514@gmail.com>
2026-09-09 22:29:31 +0530

    Build initial SpeakML React frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">SpeakML</div>
+
+      <div className="nav-links">
+        <a href="#">Model Builder</a>
+        <a href="#">Results</a>
+        <a href="#">Test Model</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
